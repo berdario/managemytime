e5883eb7f325527fffc1fb60afed3b6102b56a8e
Dario Bertini <berdario@gmail.com>
2016-08-14 21:58:26 +0100

    Add a bunch of type signatures

--- src/ManageMyTime.hs
+++ src/ManageMyTime.hs
@@ -5,10 +5,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE Rank2Types #-}
 
 module ManageMyTime where
 
-import GHC.TypeLits (Symbol)
+import GHC.TypeLits (Symbol, KnownSymbol)
 import Control.Arrow ((&&&))
 import Control.Error.Util (note, noteT)
 import Control.Monad ((<=<))
@@ -30,6 +31,7 @@ import Network.Wai (Application)
 import Servant (JSON, (:>), (:<|>)(..), Proxy(..), ServantErr(..), Capture, Headers, Header,
                 ReqBody, QueryParam, Get, Post, Put, Delete, err403, err404, err409, Raw)
 import qualified Servant
+import Servant (URI)
 import Servant.API.ResponseHeaders (addHeader)
 import Servant.Utils.Links (safeLink, MkLink, IsElem, HasLink)
 
@@ -82,10 +84,15 @@ timeAPI = Proxy
 apiLink :: (IsElem endpoint TimeAPI, HasLink endpoint) =>
             Proxy endpoint -> MkLink endpoint
 apiLink = safeLink timeAPI
+taskLink :: Key Task -> URI
 taskLink = apiLink (Proxy :: Proxy (Auth :> "task" :> Capture "id" (Key Task) :> Get '[JSON] ClientTask))
+itemLink :: Key Item -> URI
 itemLink = apiLink (Proxy :: Proxy (Auth :> "item" :> Capture "id" (Key Item) :> Get '[JSON] ClientItem))
+itemsLink :: URI
 itemsLink = apiLink (Proxy :: Proxy (Auth :> "items" :> Get '[JSON] [Entity Item]))
+profileLink :: URI
 profileLink = apiLink (Proxy :: Proxy (Auth :> "profile" :> Get '[JSON] ClientUser))
+userLink :: Key User -> URI
 userLink = apiLink (Proxy :: Proxy (Auth :> "user" :> Capture "id" (Key User) :> Get '[JSON] UserWithPerm))
 
 type AppM = ExceptT Servant.ServantErr IO
@@ -103,6 +110,8 @@ reportDuplicateError (Just duplicate) = throwE $ decorateServantError err409  $ 
 
 liftValidate :: Text -> AppM (Entity User)
 liftValidate tkn = withExceptT decorateAuthError $ ExceptT $ Auth.validate tkn
+
+validateLevel :: AuthLevel -> Text -> AppM (Entity User)
 validateLevel minLevel = (\u -> if (>= minLevel)(userAuth $ entityVal u) then (return u) else (throwE err403)) <=< liftValidate
 
 withUserFrom :: (PersistEntity val, PersistEntityBackend val ~ SqlBackend) => (Entity User -> (Key val) -> val -> AppM b) -> Text -> (Key val) -> AppM b
@@ -121,6 +130,10 @@ withTaskPerms f = withUserFrom f'
 getTask :: Text -> (Key Task) -> AppM ClientTask
 getTask = withTaskPerms (\_ _ t -> return $ taskName t)
 
+newTask :: KnownSymbol h =>
+           Text ->
+           Text ->
+           AppM (Headers '[Header h URI] (Key Task))
 newTask tkn taskname = do
   usr <- liftValidate tkn
   mNewKey <- runDb $ insertUnique $ Task taskname $ entityKey usr
@@ -138,6 +151,7 @@ updateTask tkn key newtext = do
                       runDb $ updateTaskName key newtext
                    else throwE err403
 
+deleteTask :: Text -> Key Task -> AppM ()
 deleteTask = withTaskPerms (\_ key _ -> runDb $ delete key)
 
 withItemPerms :: (Entity User -> (Key Item) -> Item -> AppM b) -> Text -> (Key Item) -> AppM b
@@ -148,6 +162,10 @@ withItemPerms f = withUserFrom f'
 getItem :: Text -> (Key Item) -> AppM ClientItem
 getItem = withItemPerms (\_ _ t -> toClientItem t)
 
+newItem :: KnownSymbol h =>
+           Text ->
+           ClientItem ->
+           AppM (Headers '[Header h URI] (Key Item))
 newItem tkn ClientItem{..} = do
   usr <- liftValidate tkn
   mNewKey <- runDb $ insertUnique $ Item{itemTaskId=toSqlKey taskid, itemUserId=entityKey usr,
@@ -167,6 +185,7 @@ updateItem tkn key ClientItem{..} = do
                                                         itemDay=date, itemDuration=duration}
                    else throwE err403
 
+deleteItem :: Text -> Key Item -> AppM ()
 deleteItem = withItemPerms (\_ key _ -> runDb $ delete key)
 
 getPreferredHours :: Text -> AppM (Maybe Int)
@@ -174,10 +193,12 @@ getPreferredHours tkn = do
   usr <- liftValidate tkn
   return $ userPreferredHours $ entityVal usr
 
+updatePreferredHours :: Text -> Int -> AppM ()
 updatePreferredHours tkn val = do
   usr <- liftValidate tkn
   runDb $ setPreferredHours (entityKey usr) (Just val)
 
+deletePreferredHours :: Text -> AppM ()
 deletePreferredHours tkn =  do
   usr <- liftValidate tkn
   runDb $ setPreferredHours (entityKey usr) Nothing
@@ -198,8 +219,10 @@ deleteProfile tkn = do
   usr <- liftValidate tkn
   runDb $ delete $ entityKey usr
 
+validateAdmin :: Text -> AppM (Entity User)
 validateAdmin = validateLevel Admin
 
+toUserWithPerm :: User -> UserWithPerm
 toUserWithPerm User{..} = UserWithPerm{username=userName, auth=userAuth, prefHours=userPreferredHours}
 
 getUser :: Text -> (Key User) -> AppM UserWithPerm
@@ -208,6 +231,11 @@ getUser tkn key = do
   target <- runDb $ get key
   maybe (throwE err404) (return . toUserWithPerm) target
 
+
+newUser :: KnownSymbol h =>
+           Text ->
+           UserWithPerm ->
+           AppM (Headers '[Header h URI] (Key User))
 newUser tkn UserWithPerm{..} = do
   validateAdmin tkn
   newuser <- liftIO $ createUser username (pack "changeme") auth prefHours
@@ -251,6 +279,9 @@ logout tkn = do
   usr <- liftValidate tkn
   liftIO $ Auth.logout $ userName $ entityVal usr
 
+register :: KnownSymbol h =>
+            Registration ->
+            AppM (Headers '[Header h URI] ())
 register Registration{..} = do
   user <- liftIO $ createUser newUserName password Normal Nothing
   mNewKey <- runDb $ insertUnique user
@@ -258,6 +289,9 @@ register Registration{..} = do
     (Just key) -> return $ addHeader profileLink ()
     Nothing -> throwE err409
 
+login :: KnownSymbol h =>
+         Registration ->
+         AppM (Headers '[Header h URI] Text)
 login Registration{..} = do
   tkn <- withExceptT decorateAuthError $ ExceptT $ Auth.login newUserName password
   return $ addHeader itemsLink tkn
@@ -292,15 +326,30 @@ authApi =  maybe (crudAccessDenied err403) taskCrud
      :<|>  maybe (throwE err403) getUsers
      :<|>  maybe (throwE err403) logout
 
+type CRUDEndPoints ty clientTy = KnownSymbol h =>
+                                 (Key ty -> AppM clientTy)
+                                 :<|> ((clientTy -> AppM (Headers '[Header h Servant.URI] (Key ty)))
+                                 :<|> ((Key ty -> clientTy -> AppM ())
+                                 :<|> (Key ty -> AppM ())))
+
+taskCrud :: Text -> CRUDEndPoints Task ClientTask
 taskCrud tkn =  getTask tkn :<|>  newTask tkn :<|> updateTask tkn :<|> deleteTask tkn
+itemCrud :: Text -> CRUDEndPoints Item ClientItem
 itemCrud tkn = getItem tkn :<|> newItem tkn :<|> updateItem tkn :<|> deleteItem tkn
+preferredHoursCrud :: Text -> AppM (Maybe Int)
+                  :<|> ((Int -> AppM ())
+                  :<|> ((Int -> AppM ())
+                  :<|> AppM ()))
 preferredHoursCrud tkn = getPreferredHours tkn :<|> updatePreferredHours tkn :<|> updatePreferredHours tkn :<|> deletePreferredHours tkn
+profileCrud :: Text -> AppM ClientUser :<|> ((ClientUser -> AppM ()) :<|> AppM ())
 profileCrud tkn = getProfile tkn :<|> updateProfile tkn :<|> deleteProfile tkn
+userCrud :: Text -> CRUDEndPoints User UserWithPerm
 userCrud tkn = getUser tkn :<|> newUser tkn :<|> updateUser tkn :<|> deleteUser tkn
 
 api :: Proxy (TimeAPI :<|> Raw)
 api = Proxy
 
+mainServer :: Servant.ServerT (TimeAPI :<|> Raw) AppM
 mainServer = server :<|> Servant.serveDirectory "frontend"
 
 app :: Application
